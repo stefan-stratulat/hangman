f95f3cfc39ef6ffc2b937cf6b8f4b52feb9e8196
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-03-04 22:54:25 +0200

    improved UI

--- main.py
+++ main.py
@@ -1,75 +1,15 @@
 import random
+from hangman_words import word_list
+from hangman_art import logo, stages
 
 game_is_on = True
-
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-
 lives = 6
 
-
-#list of words for the game
-word_list = ["aardvark", "baboon", "camel"]
-
-#Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
+#Randomly choose a word from the word_list
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+
+print(logo)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -82,6 +22,10 @@ print(display)
 while game_is_on:
     #Ask the user to guess a letter and assign their answer to a variable called guess.
     guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print(f"You've already guessed {guess}. Try again!")
+
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
@@ -89,8 +33,8 @@ while game_is_on:
             display[position] = letter
     print(f"{' '.join(display)}")
     if guess not in chosen_word:
+        print(f"{guess} in not part of the word. You lose a life!")
         lives -= 1
-        print(f"Lives left: {lives}")
     #win condition
     if "_" not in display:
         print("You win!")
