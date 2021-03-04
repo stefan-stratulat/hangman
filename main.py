18dd7e9f68cc9afb2b6d917f88fe5e416660ecd3
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-03-04 21:51:25 +0200

    display guess if correct

--- main.py
+++ main.py
@@ -10,10 +10,8 @@ chosen_word = random.choice(word_list)
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#TODO-1: - Create an empty List called display.
+
 #For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"]
-# with 5 "_" representing each letter to guess.
 
 display = ["_" for letter in chosen_word]
 print(display)
@@ -23,8 +21,8 @@ guess = input('Guess a letter: ').lower()
 
 #Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-for letter in chosen_word:
-    if letter == guess:
-        print('Right')
-    else:
-        print('Wrong')
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+
+display = [guess if letter == guess else "_" for letter in chosen_word]
+print(display)
