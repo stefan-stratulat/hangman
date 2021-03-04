75eb106d28953f4c0e92d410a2d1ac260065d143
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-03-04 22:19:53 +0200

    add loop and win condition

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 import random
 
+game_is_on = True
+
 #list of words for the game
 word_list = ["aardvark", "baboon", "camel"]
 
 #Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
-
 
 #For each letter in the chosen_word, add a "_" to 'display'.
 
 display = ["_" for letter in chosen_word]
 print(display)
 
-#Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input('Guess a letter: ').lower()
+#TODO-1: - Use a while loop to let the user guess again.
+#The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_").
+#Then you can tell the user they've won.
 
-#Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+while game_is_on:
+    #Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+    guess = input("Guess a letter: ").lower()
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(display)
+    if "_" not in display:
+        print("You win!")
+        game_is_on = False
 
-display = [guess if letter == guess else "_" for letter in chosen_word]
-print(display)
